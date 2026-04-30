7c92ece778fb1d8c4334bad3dca6902a89021426
Gabriele <dotigabriele@gmail.com>
2026-04-30 20:13:52 +0200

    add: HeuristicWeights

---
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/heuristic/HeuristicWeights.java
@@ -0,0 +1,9 @@
+package it.unibo.ai.didattica.competition.tablut.heuristic;
+
+public class HeuristicWeights {
+    public static final float ESCAPE_WEIGHT = 1.0f; // Peso per l'euristica dell'escape del re
+    public static final float KING_SAFETY_WEIGHT = 0.5f; // Peso per l'euristica della sicurezza del re
+    public static final float MATERIAL_WEIGHT = 0.5f; // Peso per l'euristica del materiale
+    public static final float MOBILITY_WEIGHT = 0.5f; // Peso per l'euristica della mobilità
+    public static final float CONTROL_WEIGHT = 0.5f; // Peso per l'euristica del controllo del centro
+}
