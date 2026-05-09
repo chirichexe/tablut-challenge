bc3eb9410fab5f73e64bce41d369ca93f9bb2b80
Gabriele <dotigabriele@gmail.com>
2026-05-09 18:11:37 +0200

    update

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/heuristic/HeuristicWeights.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/heuristic/HeuristicWeights.java
@@ -3,8 +3,8 @@ package it.unibo.ai.didattica.competition.tablut.heuristic;
 public final class HeuristicWeights {
     public static final float KING_ESCAPE_WEIGHT = 1.5f; // Peso per l'euristica dell'escape del re
     public static final float KING_SAFETY_WEIGHT = 3.0f; // Peso per l'euristica della sicurezza del re
-    public static final float MATERIAL_WEIGHT = 1.5f; // Peso per l'euristica del materiale
-    public static final float MOBILITY_WEIGHT = 2.0f; // Peso per l'euristica della mobilità
+    public static final float MATERIAL_WEIGHT = 2.0f; // Peso per l'euristica del materiale
+    public static final float MOBILITY_WEIGHT = 1.5f; // Peso per l'euristica della mobilità
     public static final float OPEN_PATH_WEIGHT = 2.0f; // Peso per l'euristica delle vie aperte fino al bordo
 
     /* Pesi per KingSafety */
@@ -14,7 +14,7 @@ public final class HeuristicWeights {
 
     /* Pesi per Material */
     public static final float MATERIAL_BLACK_VALUE = 1.0f;
-    public static final float MATERIAL_WHITE_VALUE = 1.0f;
+    public static final float MATERIAL_WHITE_VALUE = 2.0f;
 
     /* Nuove Euristiche */
     public static final float RHOMBUS_WEIGHT = 2.0f;
